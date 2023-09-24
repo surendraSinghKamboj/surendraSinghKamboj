11b17b90234338868d424776922b9d6ea7dc3a30
Surendra Singh Kamboj <116861180+surendraSinghKamboj@users.noreply.github.com>
2023-09-24 14:07:09 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Surendra Singh Kamboj",
+  montag:"532f8fc8fe11763df80b74528b91551d",
   description:
     "Welcome to the digital showcase of a versatile Full Stack Web Developer! Explore a curated collection of web projects, innovative designs, and cutting-edge technologies. Discover my passion for crafting seamless user experiences and building robust web applications. Let's connect and discuss how I can bring your digital ideas to life.",
 };
